63789b2795750c1c0be12481f5a77d440b7cbd70
chloë js <chloejs.horta@gmail.com>
2022-05-16 06:15:42 +0000

    innitaial commit different ways of creating funtions with Arow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,35 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a,b) {
+    //code block
+    return a+b;
+}
+let sum = addTwoNumbers(3,5)
+console.log(sum)
 
 // Arrow Function With Parameters
-
+const addTwoNumbers1 = (a,b) => {
+    return a+b;
+}
+let sum1 = addTwoNumbers1(3,5)
+console.log(sum1)
 // Single Line Arrow Function With Parameters
-
+const addTwoNumbers2 = (a,b) => (a+b);
+let sum2 = addTwoNumbers1(6,4)
+console.log(sum2)
 // Implicit Returns
+const saySomething = message => console.log(message);
+    saySomething('Hello There!')
+
+const sayHello = () => console.log('hello');
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultLines = () => (
+    `<p>
+    This is a multi string message!
+    </p>`
+)
+console.log(returnMultLines())
